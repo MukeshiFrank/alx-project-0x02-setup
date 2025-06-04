5e1c22ae46737f241b11f6a2c95cee3a79cc38fd
MukeshiFrank <mukeshimanaafrank@gmail.com>
2025-06-04 11:19:58 +0000

    Add pages/home.tsx and pages/about.tsx with basic content

---
+++ alx-project-0x02/pages/about.tsx
@@ -0,0 +1,11 @@
+// pages/about.tsx
+const About = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>This page contains information about our project.</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ alx-project-0x02/pages/home.tsx
@@ -0,0 +1,11 @@
+// pages/home.tsx
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the home page of our Next.js project.</p>
+    </div>
+  );
+};
+
+export default Home;
