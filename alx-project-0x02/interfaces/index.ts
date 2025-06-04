cdeb541326108ee0ce51a67b94b52ca4d993e23c
MukeshiFrank <mukeshimanaafrank@gmail.com>
2025-06-04 11:26:11 +0000

    Add reusable Card component and use it on home page

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -1,9 +1,30 @@
-// interfaces/index.ts
-
-export interface HeaderProps {
+export interface CardProps {
   title: string;
+  content: string;
 }
 
-export interface HomeProps {
-  message: string;
+export interface ButtonProps {
+  size?: "small" | "medium" | "large";
+  shape?: "rounded-sm" | "rounded-md" | "rounded-full";
+  label: string;
+  onClick: () => void;
+}
+
+export interface PostProps {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+export interface UserProps {
+  id: number;
+  name: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+  };
 }
--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,9 +1,15 @@
-// pages/home.tsx
+import React from "react";
+import Card from "../components/common/Card";
+
 const Home = () => {
   return (
     <div>
       <h1>Welcome to the Home Page</h1>
       <p>This is the home page of our Next.js project.</p>
+
+      <Card title="Card Title 1" content="This is the content for card 1." />
+      <Card title="Card Title 2" content="This is the content for card 2." />
+      <Card title="Card Title 3" content="This is the content for card 3." />
     </div>
   );
 };
